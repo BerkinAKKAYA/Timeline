45ec5da689ebc0b60dd617750aa636d92e44ab59
BerkinAkkaya <berkin_akkaya@hotmail.com>
2021-05-13 13:29:50 +0000

    fix indentation

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,116 @@
 <template>
-  <div id="app">
-    <h1>Timeline</h1>
+    <div id="app">
+        <header>
+            <h1>Timeline</h1>
 
-    <header>
-      <button @click="logout()" v-if="doc">Logout</button>
-      <button @click="login()" v-else>Login</button>
-      <button @click="saveData()" v-if="doc">Save Data</button>
-    </header>
+            <button @click="logout()" v-if="doc">Logout</button>
+            <button @click="login()" v-else>Login</button>
+        </header>
 
-    <div v-for="(data, year) in timestamps" :key="year">
-      <h4>{{ year }}</h4>
+        <div v-for="(data, year) in timestamps" :key="year">
+            <h4>{{ year }}</h4>
 
-      <p v-for="(timestamp, key) in data" :key="key">
-        <button @click="removeTimestamp(year, key)">Delete</button>
+            <p v-for="(timestamp, key) in data" :key="key">
+                <button @click="removeTimestamp(year, key)">Delete</button>
 
-        {{ timestamp.day }}/{{ timestamp.month }} |
-        {{ timestamp.title }}
-      </p>
+                {{ timestamp.day }}/{{ timestamp.month }} |
+                {{ timestamp.title }}
+            </p>
+        </div>
+
+        <div style="margin-top: 25px">
+            <input
+                type="number"
+                placeholder="Year"
+                v-model="timestampToAdd.year"
+            />
+            <input
+                type="number"
+                placeholder="Month"
+                v-model="timestampToAdd.month"
+            />
+            <input
+                type="number"
+                placeholder="Day"
+                v-model="timestampToAdd.day"
+            />
+            <input
+                type="text"
+                placeholder="Title"
+                v-model="timestampToAdd.title"
+            />
+            <button @click="addTimestamp()">Add</button>
+        </div>
     </div>
-
-    <div style="margin-top: 25px">
-      <input type="number" placeholder="Year" v-model="timestampToAdd.year" />
-      <input type="number" placeholder="Month" v-model="timestampToAdd.month" />
-      <input type="number" placeholder="Day" v-model="timestampToAdd.day" />
-      <input type="text" placeholder="Title" v-model="timestampToAdd.title" />
-      <button @click="addTimestamp()">Add</button>
-    </div>
-  </div>
 </template>
 
 <script>
 import { auth, usersCollection } from "./firebase";
 
-console.clear();
-
 export default {
-  name: "App",
-  data() {
-    return {
-      doc: null,
-      timestamps: {},
-      timestampToAdd: {},
-    };
-  },
-  methods: {
-    addTimestamp() {
-      const { year, month, day, title } = this.timestampToAdd;
-      this.timestamps[year] = this.timestamps[year] || [];
-      this.timestamps[year].push({ month, day, title });
+    name: "App",
+    data() {
+        return {
+            doc: null,
+            timestamps: {},
+            timestampToAdd: {},
+        };
     },
-    removeTimestamp(year, key) {
-      this.timestamps[year].shift(key);
-    },
-    login() {
-      auth.signInAnonymously();
-    },
-    logout() {
-      auth.signOut();
-    },
-    saveData() {
-      this.doc.set(this.timestamps);
-    },
-  },
-  created() {
-    auth.onAuthStateChanged((user) => {
-      this.uid = user && user.uid;
-      this.doc = user && usersCollection.doc(user.uid);
+    methods: {
+        addTimestamp() {
+            const { year, month, day, title } = this.timestampToAdd;
+            this.timestamps[year] = this.timestamps[year] || [];
+            this.timestamps[year].push({ month, day, title });
 
-      if (this.doc) {
-        this.doc.get().then((doc) => {
-          this.timestamps = doc.data();
+            this.saveData();
+        },
+        removeTimestamp(year, key) {
+            this.timestamps[year].shift(key);
+
+            this.saveData();
+        },
+        login() {
+            auth.signInAnonymously();
+        },
+        logout() {
+            auth.signOut();
+        },
+        saveData() {
+            this.doc.set(this.timestamps);
+        },
+    },
+    created() {
+        auth.onAuthStateChanged((user) => {
+            this.uid = user && user.uid;
+            this.doc = user && usersCollection.doc(user.uid);
+
+            if (this.doc) {
+                this.doc.get().then((doc) => {
+                    this.timestamps = doc.data();
+                });
+            }
         });
-      }
-    });
-  },
+    },
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    display: flex;
+    justify-content: center;
+}
+
+header {
+    padding: 20px;
+    max-width: 500px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+</style>
